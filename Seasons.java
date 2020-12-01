d1ecba3da6f52a45b25db9a765c2953dd33926bf
Vladyslav Korol <vladyslav.korol@ring.com>
2020-12-01 09:41:23 -0500

    Code style fixed

--- Seasons.java
+++ Seasons.java
@@ -1,43 +1,43 @@
 public class Seasons {
 
-	public enum Season {
-		WINTER, SPRING, SUMMER, AUTUMN
-		}
+    public enum Season {
+        WINTER, SPRING, SUMMER, AUTUMN
+    }
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		
-		Season season = null;
-		String month = "March";
 
-		switch (month) {
-		case "December": 
-		case "January": 
-		case "February":
-			season = Season.WINTER; 
-			break;	
-		case "March": 
-		case "April": 
-		case "May":
-			season = Season.SPRING; 
-			break;
-		case "June": 
-		case "July": 
-		case "August":
-			season = Season.SUMMER; 
-			break;
-		case "September": 
-		case "October": 
-		case "November":
-			season = Season.AUTUMN; 
-			break;
-		default:
-			System.out.println("No this month");
-		}
-		
-		System.out.println(season.toString());
-		
-	}
+        Season season = null;
+        String month = "March";
+
+        switch (month) {
+            case "December":
+            case "January":
+            case "February":
+                season = Season.WINTER;
+                break;
+            case "March":
+            case "April":
+            case "May":
+                season = Season.SPRING;
+                break;
+            case "June":
+            case "July":
+            case "August":
+                season = Season.SUMMER;
+                break;
+            case "September":
+            case "October":
+            case "November":
+                season = Season.AUTUMN;
+                break;
+            default:
+                System.out.println("No this month");
+        }
+
+        System.out.println(season.toString());
+
+    }
 
 }
